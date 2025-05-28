6063adce9258390fac3cd280ee677736f99ee3d9
Hossein Mashayekhi <hosseinmashayekhi20@gmail.com>
2025-05-28 09:36:53 +0330

    Add User login/signup page -add sidebar

--- src/run.py
+++ src/run.py
@@ -1,5 +1,28 @@
 import streamlit as st
 
-st.title(":zap: hello")
-st.code("x = 12")
-st.text(" bye")
+st.title(":zap: Hossein Dashboard")
+st.text(" welcome")
+
+# Login
+login_option = st.sidebar.radio('Login/Singup', ('Login', 'Singup'))
+if login_option == 'Login':
+    with st.sidebar.form("login"):
+        st.write("Login Here.")
+        username = st.text_input("Username")
+        password = st.text_input("Password", type="password")
+
+        # Every form must have a submit button.
+        submitted = st.form_submit_button("Login")
+        if submitted:
+            pass
+else:
+    with st.sidebar.form("signup"):
+        st.write("Singup Here.")
+        username = st.text_input("Username")
+        password = st.text_input("Password", type="password")
+        email = st.text_input("Email")
+
+        # Every form must have a submit button.
+        submitted = st.form_submit_button("Singup")
+        if submitted:
+            pass
